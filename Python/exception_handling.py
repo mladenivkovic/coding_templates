dda0a56c8f3d4aeff26872c630799295a67c4a9b
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-08-04 18:05:01 +0200

    exception_handling.py: added check if variable exists

--- Python/exception_handling.py
+++ Python/exception_handling.py
@@ -5,6 +5,8 @@
 #Fehler Erwarten
 
 import sys
+
+# Value errors:
 try:
     my_var = float(raw_input("Enter a number: "))
 except ValueError:
@@ -12,3 +14,35 @@ except ValueError:
     sys.exit()
     
 print 'Your number was:', my_var
+
+
+#See if variable is assigned:
+print
+print 'Checking if variable is assigned'
+
+a=5
+if 'a' in locals():
+    print 'a is local variable'
+else:
+    print 'a is not local variable'
+
+if 'a' in globals():
+    print 'a is global variable'
+else:
+    print 'a is not global variable'
+
+
+if 'b' in locals():
+    print 'b is local variable'
+else:
+    print 'b is not local variable'
+
+if 'b' in globals():
+    print 'b is global variable'
+else:
+    print 'b is not global variable'
+
+
+#To check if an object has an attribute:
+#if hasattr(obj, 'attr_name'):
+  # obj.attr_name exists.
