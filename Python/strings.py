b4f20a9f0649b3329dd00a32d8b2c7a78d527b94
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-10-19 15:19:18 +0200

    Initial commit string manipulation

---
+++ Python/strings.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+
+mystring="Hallo. Das ist ein beliebiger String."
+
+print "my string:", mystring
+
+#Last 7 characters of String:
+print mystring[-7:]
+
+#Original String wasn't cut off:
+print mystring
+
+#All but the last character
+print mystring[:-1]
+
+#Last character:
+print mystring[-1]
+
+#7th last character
+print mystring[-7]
