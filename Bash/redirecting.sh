5c3b041e15fcfbaa8f0ee851c6f702a28b712546
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2016-10-22 22:22:33 +0200

    redirecting: added comments.

--- Bash/redirecting.sh
+++ Bash/redirecting.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Define function f
+# First command: say hy
+# Second command: Bullshit, throws an error
+# Write Error to file stderrlog, write stdout to screen AND file stdoutlog
 
 f() {
     echo "hi"
@@ -8,3 +12,5 @@ f() {
 
 
 f 2>stderrlog | tee stdoutlog
+
+
