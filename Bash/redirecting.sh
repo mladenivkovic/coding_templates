92a05c872cc8382ff63f19b8f002f73cda469146
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-04-09 18:08:19 +0200

    redirecting: 2>&1 added

--- Bash/redirecting.sh
+++ Bash/redirecting.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define function f
-# First command: say hy
+# First command: say hi
 # Second command: Bullshit, throws an error
 # Write Error to file stderrlog, write stdout to screen AND file stdoutlog
 
@@ -13,4 +13,6 @@ f() {
 
 f 2>stderrlog | tee stdoutlog
 
+# Write output and errors to null:
 
+f > /dev/null 2>&1 
