2daf752e483deac65c2d0f80cf200c8a0021edab
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-04-09 18:13:40 +0200

    redirecting update

--- Bash/redirecting.sh
+++ Bash/redirecting.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+
+
+# Write output and errors to null:
+
+input/somescript.sh  > /dev/null 2>&1 
+
+
+
+
+
+# Also works with functions:
 # Define function f
 # First command: say hi
 # Second command: Bullshit, throws an error
@@ -13,6 +25,6 @@ f() {
 
 f 2>stderrlog | tee stdoutlog
 
-# Write output and errors to null:
 
-f > /dev/null 2>&1 
+
+
