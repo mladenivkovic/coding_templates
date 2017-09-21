67995443ac30074e58c91431fd98cb3d3c486f33
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-09-21 16:22:31 +0200

    initial commit increment_operators.c

---
+++ C/increment_operators.c
@@ -0,0 +1,31 @@
+/**/
+/** Demonstrates the usage of increment operators*/
+/**/
+
+
+#include <stdio.h>
+
+
+int main(void)
+{
+  int n = 1;
+
+  printf("   ++\n");
+  printf("n   %3d\n", n);
+  printf("++n %3d\n", ++n);
+  printf("n   %3d\n", n);
+  printf("\n");
+
+  printf("n++ %3d\n", n++);
+  printf("n   %3d\n", n);
+  printf("\n");
+
+  printf("   --\n");
+  printf("--n %3d\n", --n);
+  printf("n   %3d\n", n);
+  printf("\n");
+
+  printf("n-- %3d\n", n--);
+  printf("n   %3d\n", n);
+  printf("\n");
+}
