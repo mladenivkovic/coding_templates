610d9dee82bcf79c15babda4371c6d1e5bdffffd
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-09-28 11:55:17 +0200

    initial commit cmdlineargs; renamed readparams in make to init

---
+++ C/cmdlineargs.c
@@ -0,0 +1,28 @@
+/* 
+ * Handling command line arguments
+ */
+
+
+
+#include <stdio.h>      /* input, output    */
+#include <string.h>
+
+
+
+int
+main( int argc,     // input argument count (including program name)
+      char *argv[]  // input argument vector
+    )
+{
+  
+  printf("These were your cmd line args:\n");
+  char str[80];
+
+  for (int i = 0; i < argc; i++){
+    printf("Arg %d: %s\n", i, argv[i]);
+  }
+
+
+  return(0);
+}
+
