e611377fab643452a39215cd723e0df3d83cd259
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-09-30 00:26:38 +0200

    Initial commit openmp templates

---
+++ C/openmp/omp_environment.c
@@ -0,0 +1,57 @@
+/* 
+ * Write some comments in here.
+ * compile with -fopenmp flag (gcc)
+ */
+
+
+
+#include <stdio.h>      /* input, output    */
+#include <omp.h>        /* openMP library     */
+
+
+
+
+
+
+int
+main(void)    
+{
+
+  int nthreads, tid, procs, maxt, inpar, dynamic, nested;
+
+// start parallel region
+#pragma omp parallel
+{
+  /* Obtain thread number */
+  tid = omp_get_thread_num();
+
+  if (tid == 0){
+    printf("Thread %d getting environment info...\n", tid);
+
+    /* Get environment information */
+    procs = omp_get_num_procs();        // number of processors in use
+    nthreads = omp_get_num_threads();   // number of threads in use; hyperthreading may be enabled
+    maxt = omp_get_max_threads();       // max available threads
+    inpar = omp_in_parallel();          // is in parallel region?
+    dynamic = omp_get_dynamic();        // dynamic threads enabled
+    nested = omp_get_nested();          // nested parallelism enabled
+
+    /* Print environment information */
+    printf("Number of processors = %d\n", procs);
+    printf("Number of threads = %d\n", nthreads);
+    printf("Max threads = %d\n", maxt);
+    printf("In parallel? = %d\n", inpar);
+    printf("Dynamic threads enabled? = %d\n", dynamic);
+    printf("Nested parallelism enabled? = %d\n", nested);
+
+  }  /* Done */
+
+
+}
+// end parallel region
+#pragma end parallel
+
+
+  return(0);
+}
+
---
+++ C/openmp/starting_template_openmp.c
@@ -0,0 +1,22 @@
+/* 
+ * Write some comments in here.
+ * compile with -fopenmp flag (gcc)
+ */
+
+
+
+#include <stdio.h>      /* input, output    */
+#include <omp.h>        /* openMP library     */
+
+
+
+
+
+
+int
+main(void)    
+{
+
+  return(0);
+}
+
