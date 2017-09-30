e170c8b2b98a981a4c723fe1f48eb4772f4b01a7
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-09-30 18:32:22 +0200

    omp basics cleanup

--- C/openmp/omp_basics.c
+++ C/openmp/omp_basics.c
@@ -17,24 +17,10 @@ int
 main(void)    
 {
 
-  int nthreads, tid, procs, maxt, inpar, dynamic, nested;
+  int tid, inpar;
 
 
-/*  [> Get environment information <]*/
-  /*procs = omp_get_num_procs();        // number of processors in use*/
-  /*nthreads = omp_get_num_threads();   // number of threads in use; hyperthreading may be enabled*/
-  /*maxt = omp_get_max_threads();       // max available threads*/
   inpar = omp_in_parallel();          // is in parallel region?
-  /*dynamic = omp_get_dynamic();        // dynamic threads enabled*/
-  /*nested = omp_get_nested();          // nested parallelism enabled*/
-  /**/
-  /*[> Print environment information <]*/
-  /*printf("Number of processors = %d\n", procs);*/
-  /*printf("Number of threads = %d\n", nthreads);*/
-  /*printf("Max threads = %d\n", maxt);*/
-  /*printf("Dynamic threads enabled? = %d\n", dynamic);*/
-  /*printf("Nested parallelism enabled? = %d\n", nested);*/
-/**/
   printf("Not in parallel region.\n");
   printf("In parallel? = %d\n", inpar);
   
@@ -60,10 +46,7 @@ main(void)
 
 
 
-
-
-}
-// end parallel region
+} // end of parallel region
 
 
   return(0);
