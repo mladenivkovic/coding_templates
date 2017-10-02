1974942d3e8f04283ce3908d5847e332a83cf1f4
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-10-02 15:21:25 +0200

    backup/share commit workshare

--- C/openmp/omp_worksharing.c
+++ C/openmp/omp_worksharing.c
@@ -14,14 +14,178 @@
 
 
 
+void forloop(void);
+void reduction(void);
+void scheduling(void);
+
+
+
+
+
 int
 main(void)    
 {
 
+  /* worksharing: for loop */
+  forloop();
+
+  /* for loop for reduction */
+  /*reduction();*/
+
+  /* different scheduling */
+  /*scheduling();*/
+
+
+
+
+  return(0);
+}
+
+
+
+
+
+
+/*=============================================================*/
+/*=============================================================*/
+/*=============================================================*/
+
+
+
+
+void forloop(void){
+
+
+  printf("=====================\n");
+  printf("FOR LOOP\n");
+  printf("=====================\n\n");
+
+
+
+  /*======*/
   /*set up*/
-  long i, j;
+  /*======*/
+
+  long i;
+
+  clock_t start, end;
+  double cpu_time_used;
+
+  static double sourcearray[N];
+
+
+
+
+
+
+
+
+  /*============*/
+  /*measure time*/
+  /*============*/
+
+  start=clock();
+  
+  for (i=0; i<N; i++){
+    sourcearray[i] = ((double) (i)) * ((double) i)/2.2034872;
+  }
+
+  end = clock();
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+  printf("Non-parallel needed %lf s\n", cpu_time_used);
+
+
+
+
+
+
+
+  /*===============*/
+  /*parallel region*/
+  /*===============*/
+
+#pragma omp parallel
+  {
+
+    double starttime_omp, endtime_omp;
+    /*time measurement*/
+    starttime_omp=omp_get_wtime();
+
+    int procs, maxt;
+
+    procs = omp_get_num_procs();        // number of processors in use
+    maxt = omp_get_max_threads();       // max available threads
+
+
+    int nthreadss = omp_get_num_threads();
+    int id = omp_get_thread_num();
+    printf("num threads forloop %d from id %d, procs: %d, maxthrds: %d\n", nthreadss, id, procs, maxt);
+
+
+
+
+#pragma omp for  
+    for (i=0; i<N; i++){
+      sourcearray[i] = ((double) (i)) * ((double) i)/2.2034872;
+    }
+
+
+
+
+    endtime_omp = omp_get_wtime();
+    cpu_time_used = ((endtime_omp - starttime_omp)) ;
+
+/*#pragma omp master*/
+/*    {*/
+/*      [> print time used <]*/
+/*      [> only thread 0 does this <]*/
+/*      int nthreads = omp_get_num_threads();*/
+/*      printf("Parallel needed %lf s with %d threads\n", cpu_time_used, nthreads);*/
+/*    }*/
+
+  } /* end parallel region */
+
+
+}
+
+
+
+
+
+
+
+
+
+/*=============================================================*/
+/*=============================================================*/
+/*=============================================================*/
+
+
+
+
+
+void reduction(void){
+
+
+  /*====================================================*/
+  /* a for loop where something is calculated and then  */
+  /* stored in one variable                             */
+  /*====================================================*/
+
+
+  printf("=====================\n");
+  printf("REDUCTION\n");
+  printf("=====================\n\n");
+
+
+
+  /*======*/
+  /*set up*/
+  /*======*/
+
+  long i;
   double summe = 0.0;
-  long produkt = 1;
 
   clock_t start, end;
   double cpu_time_used;
@@ -29,34 +193,31 @@ main(void)
 
 
   static double sourcearray[N];
-  int nsmall = 16;
-  int smallarray[nsmall];
 
   for (i=0; i<N; i++){
     sourcearray[i] = ((double) (i)) * ((double) i)/2.2034872;
   }
 
-  for (i=0; i<nsmall; i++){
-    smallarray[i] = i*i+3;
-  }
 
 
 
 
 
 
-
+  /*============*/
   /*measure time*/
+  /*============*/
+
   start=clock();
   
-  for (j=0; j<N; j++){
-    summe = summe + sourcearray[j];
+  for (i=0; i<N; i++){
+    summe = summe + sourcearray[i];
   }
 
   end = clock();
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 
-  printf(" Non-parallel needed %lf s\n", cpu_time_used);
+  printf("Non-parallel needed %lf s\n", cpu_time_used);
 
   /*reset summe*/
   summe = 0;
@@ -66,15 +227,17 @@ main(void)
 
 
 
-
+  /*===============*/
   /*parallel region*/
-#pragma omp parallel shared(summe, produkt)
+  /*===============*/
+
+#pragma omp parallel shared(summe)
   {
     /*Infinite loops and do while loops are not parallelizable with OpenMP.*/
     int id = omp_get_thread_num();
     int nthreads = omp_get_num_threads();
 
-
+    /*time measurement*/
     double starttime_omp, endtime_omp;
 
     if (id == 0){
@@ -82,10 +245,14 @@ main(void)
     }
 
 
+
+
+
+
 #pragma omp for reduction(+ : summe)
   
-    for (j=0; j<N; j++){
-      summe = summe + sourcearray[j];
+    for (i=0; i<N; i++){
+      summe = summe + sourcearray[i];
     }
   
 
@@ -96,6 +263,42 @@ main(void)
       printf("Parallel needed %lf s with %d threads\n", cpu_time_used, nthreads);
     }
 
+  }
+
+
+}
+
+
+
+
+/*=============================================================*/
+/*=============================================================*/
+/*=============================================================*/
+
+
+
+
+
+void scheduling(void){
+
+
+
+  printf("\n\n\n=====================\n");
+  printf("SCHEDULING\n");
+  printf("=====================\n");
+
+  /*========*/
+  /* set up */
+  /*========*/
+
+  int nsmall = 12;
+  int smallarray[nsmall];
+
+  for (int i=0; i<nsmall; i++){
+    smallarray[i] = i*i+3;
+  }
+
+  long produkt = 1;
 
 
 
@@ -103,24 +306,19 @@ main(void)
 
 
 
+  /*===============*/
+  /*parallel region*/
+  /*===============*/
 
-
-
-
-
-    /*SCHEDULING*/
-
-    if (id == 0){
-      printf("\n Non-scheduled reduced loop\n");
-    }
-
-  /*Tell threads to wait for all to arrive here*/
-#pragma omp barrier
+  printf("\nNon-scheduled reduced loop\n");
+#pragma omp parallel
+  {
+    int id = omp_get_thread_num();
+    int nthreads = omp_get_num_threads();
 
 #pragma omp for reduction(* : produkt)    
-    /*Loopityloop.*/
-    for (i = 0; i<nsmall; i++){
-      printf("ID %d has index %ld, value %d\n", id, i, smallarray[i]);
+    for (int i = 0; i<nsmall; i++){
+      printf("ID %d has index %d, value %d\n", id, i, smallarray[i]);
       produkt = produkt * smallarray[i];
     }
 
@@ -135,13 +333,13 @@ main(void)
 #pragma omp barrier
 
 
-#pragma omp master
+#pragma omp master /*only thread 0 does stuff*/
     {  
       produkt = 1;
     }
 #pragma omp for reduction(* : produkt) schedule(static,nsmall/nthreads)
-    for (i = 0; i<nsmall; i++){
-      printf("ID %d has index %ld, value %d\n", id, i, smallarray[i]);
+    for (int i = 0; i<nsmall; i++){
+      printf("ID %d has index %d, value %d\n", id, i, smallarray[i]);
       produkt = produkt * smallarray[i];
     }
 
@@ -153,15 +351,8 @@ main(void)
     }
     
 
-
-
-
   } /* end parallel region */
 
-    
+ 
 
-
-
-  return(0);
 }
-
