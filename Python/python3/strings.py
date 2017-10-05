30c6d2382617b26b1ecd3269b74fd37b00184e37
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-10-05 21:16:38 +0200

    splitting and joining strings added

--- Python/python3/strings.py
+++ Python/python3/strings.py
@@ -3,6 +3,13 @@
 
 mystring="Hallo. Das ist ein beliebiger String."
 
+print("")
+print("=============")
+print("Basics")
+print("=============")
+print("")
+
+
 print("my string:", mystring )
 
 #Last 7 characters of String:
@@ -19,3 +26,38 @@ print(mystring[-1] )
 
 #7th last character
 print(mystring[-7] )
+
+
+
+
+
+print("")
+print("================")
+print("join and split")
+print("================")
+print("")
+
+
+
+s = '50, 16, , 12'
+l1 = s.split(',')
+
+print("splitting strings")
+print("s = '50, 16, , 12'")
+print("l1 = s.split(',')")
+print("l1 = ", l1)
+print("")
+
+
+l = ['12', '53', '24']
+s1 = ".".join(l)
+s2 = "*".join(l)
+
+
+
+print("joining strings")
+print("l = ['12', '53', '24']")
+print('s1 = ".".join(l)')
+print('s2 = "*".join(l)')
+print('s1 = ', s1)
+print('s2 = ', s2)
