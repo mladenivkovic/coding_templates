a4a823d33a47a7aa50dbcc6fa61434e556a4e517
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-10-10 21:55:41 +0200

    initial commit dicts

---
+++ Python/python3/dictionnaries.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+#=======================
+# A dictionnary how-to.
+#=======================
+
+
+#main principle: dict{key:value}
+
+
+d1 = {}
+d1["John"] = "076 1234567"
+print("dict 1:", d1)
+
+d2 = {"Mary":"076 9876543"}
+d2["Lincoln"]= "076 1357911"
+d2["Nenad"]= "076 24681012"
+print("dict 2:", d2)
+
+#Note: Dictionnaries are not ordered!
+
+
+
+print()
+print("d2.keys")
+print(d2.keys())
+print("d2.values")
+print(d2.values())
+
+print()
+del d2["Mary"]
+print('del d2["Mary"]')
+print('d2 =', d2)
+
+d2.clear()
+print("d2.clear(); d2=", d2)
+
+print("Nenad in d2", "Nenad" in d2)
+
+
