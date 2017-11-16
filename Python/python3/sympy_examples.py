33e819eb6491a34b19d3b124780bc3966ad5a04f
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-11-16 01:03:35 +0100

    initial commit sympy

---
+++ Python/python3/sympy_examples.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+import sympy
+
+a,b = sympy.symbols('a b')
+
+
+print()
+print("Expand")
+print(sympy.expand((a+b)**3))
+
+print()
+print("Factorize")
+print(sympy.factor(a**2-b**2))
+
+
+print()
+print("Differentiate")
+x = sympy.symbols('x')
+print(sympy.diff(sympy.sin(x), x))
+
+
+
+print()
+print("Integrate")
+print(sympy.integrate(x**2,(x,a,b)))
