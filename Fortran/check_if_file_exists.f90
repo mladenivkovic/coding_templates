b9c5c95e84d39cbe42d765ac2d2e4ef15444fe77
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2017-11-21 02:16:55 +0100

    initial commit check if file exists fortran

---
+++ Fortran/check_if_file_exists.f90
@@ -0,0 +1,22 @@
+program check_if_file_exists
+
+  implicit none
+
+
+  logical:: file_exists = .false.
+  logical:: dir_exists = .false.
+
+  character(len=10) :: filename = 'loops.f90'
+  character(len=10) :: dirname = 'MPI/'
+
+  inquire(file=TRIM(filename), exist=file_exists)
+
+  inquire(file=TRIM(dirname), exist=dir_exists)  
+
+
+  write(*,*) "File ", filename, "exists?", file_exists
+  write(*,*) "Directory ", dirname, "exists?", dir_exists
+ 
+ 
+
+end program check_if_file_exists
