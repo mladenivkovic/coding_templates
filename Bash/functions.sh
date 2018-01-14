b435d20c35bf41a48e7794515c9dcdbd29db1847
Mladen Ivkovic <mladen.ivkovic@uzh.ch>
2018-01-14 14:29:46 +0100

    initial commit bash functions

---
+++ Bash/functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#----------------------
+# Defining function
+#----------------------
+
+myfunction() {
+    nargs=$# #store initial number of args; shift reduces $#
+    echo "I was given" $nargs "arguments"
+    for ((i=0; i<$nargs; i++)); do
+        echo "argument "$i " is" $1
+        shift
+    done;
+}
+
+
+
+
+#----------------------
+# Calling function
+#----------------------
+
+myfunction 2 asd 5 3kj hihi
+
+
+
+
+
+
+
