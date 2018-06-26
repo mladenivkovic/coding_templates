989efa63e57852c2a8b87c55ade6332d22a5bdb0
mivkov <mladen.ivkovic@hotmail.com>
2018-06-26 22:46:49 +0200

    namelists: added shorthand for array input

--- Fortran/namelists/small_example/smallnamelistexample.f90
+++ Fortran/namelists/small_example/smallnamelistexample.f90
@@ -1,39 +1,41 @@
 program smallnamelist
 
-    implicit none
+  implicit none
 
-    real :: somerealone, somerealtwo, somerealthree
-    integer :: someint, someotherint
-    real, dimension(4) :: somerealarray
-    integer, dimension(3) :: someintarray
-
-
-    ! declare what part of the namelists will be stored where
-    namelist /nmlint/ someint, someotherint
-    namelist /nmlreal/ somerealone, somerealtwo, somerealthree
-    namelist /nmlarray/ somerealarray, someintarray
+  real :: somerealone, somerealtwo, somerealthree
+  integer :: someint, someotherint
+  real, dimension(4) :: somerealarray
+  integer, dimension(3) :: someintarray
+  integer, dimension(3) :: otherintarray
 
 
+  ! declare what part of the namelists will be stored where
+  namelist /nmlint/ someint, someotherint
+  namelist /nmlreal/ somerealone, somerealtwo, somerealthree
+  namelist /nmlarray/ somerealarray, someintarray, otherintarray
 
 
-    open(1, file='input.nml', status='old')
-
-    write(6, *)
-    write(6, *) "Reading in NMLINT"
-    read(1, nml=nmlint)
-    write(6, *) someint, someotherint
 
 
-    write(6, *)
-    write(6, *) "Reading in NMLREAL"
-    read(1, nml=nmlreal)
-    write(6, *) somerealone, somerealtwo, somerealthree
+  open(1, file='input.nml', status='old')
 
-    write(6, *)
-    write(6, *) "Reading in NMLARRAY"
-    read(1, nml=nmlarray) 
-    write(6, *) somerealarray
-    write(6, *) someintarray
+  write(6, *)
+  write(6, *) "Reading in NMLINT"
+  read(1, nml=nmlint)
+  write(6, *) someint, someotherint
+
+
+  write(6, *)
+  write(6, *) "Reading in NMLREAL"
+  read(1, nml=nmlreal)
+  write(6, *) somerealone, somerealtwo, somerealthree
+
+  write(6, *)
+  write(6, *) "Reading in NMLARRAY"
+  read(1, nml=nmlarray) 
+  write(6, *) somerealarray
+  write(6, *) someintarray
+  write(6, *) otherintarray
 
 
 
