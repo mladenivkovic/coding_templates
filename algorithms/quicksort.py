cf94d13b349246fa68ef1c130733f079b10a41ae
mivkov <mladen.ivkovic@hotmail.com>
2018-10-18 19:05:42 +0200

    added quicksort

---
+++ algorithms/quicksort.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+#  arr = [7,4,2,6,3,4,9,13,1,22,43,243]
+from random import randint, seed
+seed(1)
+arr = [randint(0,100) for i in range(100)]
+
+
+def quicksort(a, lo, hi):
+
+    if lo < hi:
+
+        # pick a pivot
+        pivot = a[(lo+hi)//2]
+
+        # partition array
+        i = lo 
+        j = hi
+        
+        # loop until i>=j
+        while i<=j:
+        
+            # loop until you found smaller than pivot
+            while a[i] < pivot:
+                i += 1
+      
+            # loop until you found bigger than pivot
+            while a[j] > pivot:
+                j -= 1
+
+            # swap every instance you found
+            if i<=j:
+                temp = a[i]
+                a[i] = a[j]
+                a[j] = temp
+                i += 1
+                j -= 1
+
+        # call recursively
+        quicksort(a, lo, j)
+        quicksort(a, i, hi)
+
+
+print("Original array")
+print(arr)
+quicksort(arr,0,len(arr)-1)
+print("Array sorted in place:")
+print(arr)
