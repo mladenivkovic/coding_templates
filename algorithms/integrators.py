9dd93aa2b13d7380beb5330e80959fd88564ef63
mivkov <mladen.ivkovic@hotmail.com>
2018-10-22 15:36:42 +0200

    update integrators

--- algorithms/integrators.py
+++ algorithms/integrators.py
@@ -17,117 +17,116 @@ ax3.set_ylabel('x')
 ax4 = fig.add_subplot(324)
 ax4.set_title(r'Relative difference $\frac{y-y_{analytical}}{y_{analytical}}$')
 ax5 = fig.add_subplot(325)
-#  ax5.set_title(r'Integrating $\frac{d^2x}{dt^2} = -1$, $v_0 = 1$')
 ax5.set_xlabel('t')
 ax5.set_ylabel('v')
 ax6 = fig.add_subplot(326)
 ax6.set_title(r'Relative difference $\frac{y-y_{analytical}}{y_{analytical}}$')
 
 
-#  #====================================================================
-#  # Integrate dy/dx = cos(x)*y numerically using different methods
-#  #====================================================================
-#
-#
-#  xmin = 0
-#  xmax = 2*np.pi
-#  nx = 50
-#  x = np.linspace(xmin, xmax, nx)
-#  dx = x[1] - x[0]
-#
-#  def dydx(y,x):
-#      return np.cos(x)*y
-#
-#  def analytical(x):
-#      return np.exp(np.sin(x))
-#
-#  analy = analytical(x)
-#
-#
+#====================================================================
+# Integrate dy/dx = cos(x)*y numerically using different methods
+#====================================================================
 
-#
-#
-#
-#
-#  #============================
-#  # EXPLICIT EULER
-#  #============================
-#  y = np.zeros(nx)
-#  y[0] = 1
-#  for n in range(nx-1):
-#      y[n+1] = y[n] + dydx(y[n],x[n])*dx
-#  ax1.plot(x, y, label='explicit euler')
-#  ax2.plot(x, y/analy-1, label='explicit euler')
-#
-#
-#
-#  #============================
-#  # IMPLICIT EULER
-#  #============================
-#  y = np.zeros(nx)
-#  y[0] = 1
-#  for n in range(nx-1):
-#      y[n+1] = y[n] + dydx(analytical(x[n+1]), x[n+1])*dx
-#  ax1.plot(x, y, label='implicit euler')
-#  ax2.plot(x, y/analy-1, label='implicit euler')
-#  xmax = 2
-#
-#
-#
-#  #============================
-#  # IMPLICIT MIDPOINT EULER
-#  #============================
-#  y = np.zeros(nx)
-#  y[0] = 1
-#  for n in range(nx-1):
-#      y[n+1] = y[n] + dydx(0.5*(analytical(x[n+1])+y[n]), 0.5*(x[n]+x[n+1]))*dx
-#  ax1.plot(x, y, label='implicit midpoint')
-#  ax2.plot(x, y/analy-1, label='implicit midpoint')
-#
-#
-#
-#
-#  #============================
-#  # RUNGE KUTTA 2
-#  #============================
-#  y = np.zeros(nx)
-#  y[0] = 1
-#  for n in range(nx-1):
-#      k1 = dydx(y[n],x[n])*dx
-#      k2 = dydx(y[n]+k1, x[n+1])*dx
-#      y[n+1] = y[n] +0.5*(k1+k2)
-#  ax1.plot(x, y, label='Runge Kutta 2')
-#  ax2.plot(x, y/analy-1, label='Runge Kutta 2')
-#
-#
-#
-#
-#  #============================
-#  # RUNGE KUTTA 4
-#  #============================
-#  y = np.zeros(nx)
-#  y[0] = 1
-#  for n in range(nx-1):
-#      k1 = dydx(y[n],x[n])*dx
-#      k2 = dydx(y[n]+k1/2, x[n]+dx/2)*dx
-#      k3 = dydx(y[n]+k2/2, x[n]+dx/2)*dx
-#      k4 = dydx(y[n]+k3, x[n]+dx)*dx
-#      y[n+1] = y[n]+(k1/6 + k2/3 + k3/3 + k4/6)
-#  ax1.plot(x, y, label='Runge Kutta 4')
-#  ax2.plot(x, y/analy-1, label='Runge Kutta 4')
-#
-#
-#
-#
-#
-#
-#  x = np.linspace(xmin, xmax, 1000)
-#  ax1.plot(x, analytical(x), label='analytical solution')
-#
-#  ax1.legend()
-#  ax2.legend()
-#  ax1.set_xlim(xmin, xmax)
-#
+
+xmin = 0
+xmax = 2*np.pi
+nx = 50
+x = np.linspace(xmin, xmax, nx)
+dx = x[1] - x[0]
+
+def dydx(y,x):
+    return np.cos(x)*y
+
+def analytical(x):
+    return np.exp(np.sin(x))
+
+analy = analytical(x)
+
+
+
+
+
+
+
+#============================
+# EXPLICIT EULER
+#============================
+y = np.zeros(nx)
+y[0] = 1
+for n in range(nx-1):
+    y[n+1] = y[n] + dydx(y[n],x[n])*dx
+ax1.plot(x, y, label='explicit euler')
+ax2.plot(x, y/analy-1, label='explicit euler')
+
+
+
+#============================
+# IMPLICIT EULER
+#============================
+y = np.zeros(nx)
+y[0] = 1
+for n in range(nx-1):
+    y[n+1] = y[n] + dydx(analytical(x[n+1]), x[n+1])*dx
+ax1.plot(x, y, label='implicit euler')
+ax2.plot(x, y/analy-1, label='implicit euler')
+xmax = 2
+
+
+
+#============================
+# IMPLICIT MIDPOINT EULER
+#============================
+y = np.zeros(nx)
+y[0] = 1
+for n in range(nx-1):
+    y[n+1] = y[n] + dydx(0.5*(analytical(x[n+1])+y[n]), 0.5*(x[n]+x[n+1]))*dx
+ax1.plot(x, y, label='implicit midpoint')
+ax2.plot(x, y/analy-1, label='implicit midpoint')
+
+
+
+
+#============================
+# RUNGE KUTTA 2
+#============================
+y = np.zeros(nx)
+y[0] = 1
+for n in range(nx-1):
+    k1 = dydx(y[n],x[n])*dx
+    k2 = dydx(y[n]+k1, x[n+1])*dx
+    y[n+1] = y[n] +0.5*(k1+k2)
+ax1.plot(x, y, label='Runge Kutta 2')
+ax2.plot(x, y/analy-1, label='Runge Kutta 2')
+
+
+
+
+#============================
+# RUNGE KUTTA 4
+#============================
+y = np.zeros(nx)
+y[0] = 1
+for n in range(nx-1):
+    k1 = dydx(y[n],x[n])*dx
+    k2 = dydx(y[n]+k1/2, x[n]+dx/2)*dx
+    k3 = dydx(y[n]+k2/2, x[n]+dx/2)*dx
+    k4 = dydx(y[n]+k3, x[n]+dx)*dx
+    y[n+1] = y[n]+(k1/6 + k2/3 + k3/3 + k4/6)
+ax1.plot(x, y, label='Runge Kutta 4')
+ax2.plot(x, y/analy-1, label='Runge Kutta 4')
+
+
+
+
+
+
+x = np.linspace(xmin, xmax, 1000)
+ax1.plot(x, analytical(x), label='analytical solution')
+
+ax1.legend()
+ax2.legend()
+ax1.set_xlim(xmin, xmax)
+
 
 
 
