a79729d83482f2263af0f2bf1c6e05d6096e6ac1
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2018-12-21 09:50:09 +0100

    python warnings to errors

---
+++ Python/python3/warnings_to_errors.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#==============================
+#
+# Turn warnings into errors.
+#
+#==============================
+
+from numpy import array
+
+a = array([2, 3, 0, 4])
+b = array([1, 3, 4, 2])
+
+print(b/a)
+
+import warnings
+warnings.simplefilter("error")
+
+print(b/a)
