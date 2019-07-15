ad8ce395926d460bd2f64a675821a6442d5127a9
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-07-15 16:10:56 +0200

    bash ifs

---
+++ bash/ifs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#--------------------------------------------
+# Short demonstration for ifs and elses
+#--------------------------------------------
+
+
+
+if [ "foo" == "foo" ]; then
+    echo "true"
+else
+    echo "false"
+fi
+
+
