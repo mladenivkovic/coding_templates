c644ed46515ae4d1a31722d579a5e7c28a35fd90
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-07-25 16:36:30 +0200

    pthreads updates

---
+++ c/PThreads/passing_arguments_to_threads.c
@@ -0,0 +1,73 @@
+/* ================================================================================ 
+ * COMPILE WITH gcc -pthread simple_multithreads.c                                  
+ *
+ *  Pass multiple arguments to dynamically generated threads.
+ * ================================================================================ */
+
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+/* define struct to pass args */
+typedef struct{
+  int id;
+  int val1;
+  float val2;
+  char * string;
+} argstruct;
+
+
+void * do_one_thing(void *args)
+{
+  int id, val1;
+  float val2;
+  char * string;
+
+  argstruct *a = (argstruct *) args;
+
+  id = a->id;
+  val1 = a->val1;
+  val2 = a->val2;
+  string = a->string;
+
+  printf("thread %d got %d, %f and '%s'\n", id, val1, val2, string);
+
+
+  return NULL;
+}
+
+extern int
+main(void)
+{
+  
+
+  int N = 4;
+
+  pthread_t * threadlist = malloc(N*sizeof(pthread_t));
+  argstruct ** arglist = malloc(N*sizeof(argstruct*));
+
+  for(int t = 0; t<N; t++){
+    argstruct *a = malloc(sizeof(argstruct*));
+    /* fill struct with junk args to show off*/
+    a->id = t;
+    a->val1 = (t+1)*2;
+    a->val2 = (float) (10*(t+1))/7.3;
+    a->string = "Hello World";
+    arglist[t] = a; 
+    pthread_create(&threadlist[t],  /* pointer to a buffer for thread "ID"                                         */
+            NULL,                   /* pointer to a thread attribute object                                        */
+            do_one_thing,           /* pointer to routine at which new thread will start executing                 */
+            (void *) arglist[t]);   /* pointer to parameter to be passed to the routine at which new thread starts */
+  }
+
+
+
+  for(int t = 0; t<N; t++){
+    pthread_join(threadlist[t],  NULL);
+  }
+
+  return 0;
+}
+
+
---
+++ c/PThreads/spawning_recklessly.c
@@ -0,0 +1,68 @@
+/* ================================================================================ 
+ * COMPILE WITH gcc -pthread simple_multithreads.c                                  
+ *
+ *  Try to spawn as many threads as you can. Let's see how many we can get.
+ * ================================================================================ */
+
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+/* define struct to pass args */
+typedef struct{
+  int id;
+} argstruct;
+
+
+void * do_one_thing(void *args)
+{
+  int i, j, x, id;
+
+  argstruct *a = (argstruct *) args;
+
+  id = a->id;
+
+  printf("thread %d doing one thing \n", id);
+
+  for (i = 0;    i  <  1000;  i++) {
+    for (j = 0; j < 10000000; j++) x = x + i;
+  }
+
+  printf("thread %d finished doing one thing.\n", id);
+
+  return NULL;
+}
+
+extern int
+main(void)
+{
+  
+
+  printf("This program is meant to be overdoing things and take a while so you can top in the meantime and check out what happens.\n");
+
+  int N = 20;
+
+  pthread_t * threadlist = malloc(N*sizeof(pthread_t));
+  argstruct ** arglist = malloc(N*sizeof(argstruct*));
+
+  for(int t = 0; t<N; t++){
+    argstruct *a = malloc(sizeof(argstruct*));
+    a->id = t;
+    arglist[t] = a; 
+    pthread_create(&threadlist[t],  /* pointer to a buffer for thread "ID"                                         */
+            NULL,                   /* pointer to a thread attribute object                                        */
+            do_one_thing,           /* pointer to routine at which new thread will start executing                 */
+            (void *) arglist[t]);   /* pointer to parameter to be passed to the routine at which new thread starts */
+  }
+
+
+
+  for(int t = 0; t<N; t++){
+    pthread_join(threadlist[t],  NULL);
+  }
+
+  return 0;
+}
+
+
