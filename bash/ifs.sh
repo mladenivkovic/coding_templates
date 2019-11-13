13293bb58c2958dbbed32912de0df2c763e086b5
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-11-13 17:59:41 +0100

    additions to bash ifs

--- bash/ifs.sh
+++ bash/ifs.sh
@@ -13,3 +13,11 @@ else
 fi
 
 
+# one-liners for if [var=true] do stuff:
+# var needs to be either defined or undefined, what value you set doesn't matter
+
+
+do_check=asdjhasidh
+[ $do_check ] && echo doing check first time
+do_check=
+[ $do_check ] && echo doing check second time
