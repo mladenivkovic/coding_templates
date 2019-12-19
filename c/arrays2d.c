f3ae9a319d988eca7ab47e4424320d16175c9dcb
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2019-12-19 12:00:30 +0100

    c 2d arrays update

--- c/arrays2d.c
+++ c/arrays2d.c
@@ -7,6 +7,11 @@
 #include <stdlib.h> /* used for allocation stuff */
 
 
+struct mystruct{
+  float ** some2darr;
+};
+
+
 //====================
 int main(void)
 //====================
@@ -33,6 +38,26 @@ int main(void)
     }
   }
 
+  /* Inside a struct */
+  struct mystruct ms;
+  ms.some2darr = malloc(n*sizeof(float*));
+  for (int i = 0; i<n; i++){
+    ms.some2darr[i] = malloc(m * sizeof(float));
+  }
+  /* assignment version 1 */
+  /* for (int i = 0; i < n; i++){ */
+  /*   for (int j = 0; j < m; j++){ */
+  /*     ms.some2darr[i][j] = 10.f * i + 1.f * j;  */
+  /*   } */
+  /* } */
+  /* assignment version 2 */
+  for (int i = 0; i < n; i++){
+    float * arr = ms.some2darr[i];
+    for (int j = 0; j < m; j++){
+      arr[j] = 10.f * i + 1.f * j; 
+    }
+  }
+
 
 
 
@@ -56,6 +81,16 @@ int main(void)
   for (int i = 0; i < n; i++){
     for (int j = 0; j < m; j++) {
       printf("%10.3f ", multi_malloc[i][j]);
+    }
+    printf("\n");
+  }
+
+  printf("----------------------\n");
+
+
+  for (int i = 0; i < n; i++){
+    for (int j = 0; j < m; j++) {
+      printf("%10.3f ", ms.some2darr[i][j]);
     }
     printf("\n");
   }
