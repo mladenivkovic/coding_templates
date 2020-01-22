e7bc7c995e17079e8c90d0e415d76b51050d9c3b
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2020-01-22 19:48:46 +0100

    added checking for trailing comments

--- c/project_template/src/io.c
+++ c/project_template/src/io.c
@@ -17,7 +17,7 @@ void io_read_cmdlineargs(int argc, char* argv[], params* p) {
     printf(
         "Too few arguments given. Run this program with PROGRAMNAME paramfile "
         "datafile\n");
-    exit(600);
+    exit(2);
   } else {
 
     strcpy(g->paramfilename, argv[1]);
@@ -40,33 +40,21 @@ void io_read_paramfile(params* p) {
   /* open file */
   FILE* par = fopen(g->paramfilename, "r");
 
-  if (par == NULL) {
-    printf("Error: file '%s' not found.\n", g->paramfilename);
-    exit(602);
-  }
-
-  char varname[80];
-  char varvalue[80];
+  char varname[MAX_LINE_SIZE];
+  char varvalue[MAX_LINE_SIZE];
   char tempbuff[MAX_LINE_SIZE];
 
-  while (fgets(tempbuff, MAX_LINE_SIZE, par))
-  /* fgets(str_buff, n,filepointer) : */
-  /* gets n characters from file in filepointer and stores them */
-  /* in str_buff. */
-  /* returns 0 if EoF is reached. */
-
-  {
+  while (fgets(tempbuff, MAX_LINE_SIZE, par)) {
 
     if (line_is_empty(tempbuff)) continue;
 
     sscanf(tempbuff, "%20s = %56[^\n]\n", varname, varvalue);
 
     if (line_is_comment(varname)) continue;
+    remove_trailing_comments(varvalue);
 
     if (strcmp(varname, "verbose") == 0) {
       g->verbose = atoi(varvalue);
-      /* atoi/atof: convert string to integer/float */
-      /* from stdlib.h */
     } else if (strcmp(varname, "levelmax") == 0) {
       g->levelmax = atoi(varvalue);
     } else if (strcmp(varname, "nstepmax") == 0) {
@@ -137,5 +125,27 @@ int line_is_comment(char* line) {
     return (1);
   } else {
     return (0);
+  }
+}
+
+void remove_trailing_comments(char* line) {
+  /*---------------------------------------------------------
+   * Check whether there are trailing comments in this line
+   * and if so, remove them.
+   * --------------------------------------------------------*/
+
+  for (int i = 0; i < MAX_LINE_SIZE - 2; i++) {
+    /* -2: 1 for \0 char, 1 because comment is always 2 characters long,
+     * and I only check for the first.*/
+    if (line[i] == '\0') {
+      break;
+    } else if (line[i] == '/') {
+      char twochars[3];
+      strncpy(twochars, line + i, 2);
+      if (line_is_comment(twochars)) {
+        line[i] = '\0';
+        break;
+      }
+    }
   }
 }
--- c/project_template/src/io.h
+++ c/project_template/src/io.h
@@ -13,5 +13,6 @@ void io_read_paramfile(params* p);
 void io_check_file_exists(char* fname);
 int line_is_empty(char* line);
 int line_is_comment(char* line);
+void remove_trailing_comments(char* line);
 
 #endif
