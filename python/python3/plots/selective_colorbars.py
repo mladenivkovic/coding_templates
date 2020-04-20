120c16dff1784101ce8fe9a16b6155776934cffa
Mladen Ivkovic <mladen.ivkovic@hotmail.com>
2020-04-20 14:50:02 +0200

    removed meshless import

--- python/python3/plots/selective_colorbars.py
+++ python/python3/plots/selective_colorbars.py
@@ -10,7 +10,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors
 from mpl_toolkits.axes_grid1 import make_axes_locatable, axes_size, ImageGrid
-import meshless as ms
 
 
 
