de4d892ff60505139a7f0d106bbf5bee0c3e98ce
mladenivkovic <mladen.ivkovic@hotmail.com>
2023-03-20 19:29:37 +0000

    started new cpp tutorials/templates

---
+++ cpp/cpp_new/hello_world.cpp
@@ -0,0 +1,30 @@
+#include <iostream> // IO library
+#include <string>   // string type
+
+// using namespace std; // skip this for now to explicitly trace where you get things from
+
+
+void pr_hello_world(){
+  std::cout << "Hello world!" << std::endl;
+}
+
+void pr_message(std::string s = "Default string"){
+  std::cout << s << std::endl;
+}
+
+void pr_messge_with_printf(std::string s = "Default printf string"){
+
+  printf("%s\n", s.c_str());
+}
+
+int main(){
+
+  pr_hello_world();
+  pr_message();
+  pr_message("Hello World without default argument!");
+  pr_messge_with_printf("Hello world with printf!");
+
+  return 0;
+}
+
+
---
+++ cpp/cpp_new/printing_to_stdout.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // IO library
+#include <string>   // string type
+
+// using namespace std; // skip this for now to explicitly trace where you get things from
+
+int main(){
+
+
+  // Using iostream
+  std::cout << "Hello world!\n";
+  std::cout << "Hello world! with std::endl" << std::endl; // note that std::endl flushes the buffer!
+                                                           // Also note: std::endl is *manipulator*
+
+  /* TODO: formatting */
+
+  return 0;
+}
+
+
