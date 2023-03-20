88cbf7c68728d1b19c2ebc4bba18d11c0daf9584
mladenivkovic <mladen.ivkovic@hotmail.com>
2023-03-20 23:27:00 +0000

    added simple types limits

---
+++ cpp/simple_types_limits.cpp
@@ -0,0 +1,41 @@
+#include <iostream> // IO library
+#include <climits>
+#include <limits>
+#include <float.h>
+
+int main(){
+
+  // these need #include <climits>
+  std::cout << "bits per char:          " << CHAR_BIT << std::endl;
+  std::cout << "signed char minimum:    " << SCHAR_MIN << std::endl;
+  std::cout << "signed char maximum:    " << SCHAR_MAX << std::endl;
+  // std::cout << "signed char maximum:    " << UCHAR_MIN << std::endl;
+  std::cout << "unsigned char maximum:  " << UCHAR_MAX << std::endl;
+  std::cout << "int minimum:            " << INT_MIN << std::endl;
+  std::cout << "int maximum:            " << INT_MAX << std::endl;
+  std::cout << "unsigned int maximum:   " << UINT_MAX << std::endl;
+  std::cout << "long minimum:           " << LONG_MIN << std::endl;
+  std::cout << "long maximum:           " << LONG_MAX << std::endl;
+  std::cout << "unsigned long maximum:  " << ULONG_MAX << std::endl;
+  std::cout << std::endl;
+
+  // this needs #include <float>
+  std::cout << "float epsilon:          " << FLT_EPSILON << std::endl;
+  std::cout << "float min:              " << FLT_MIN << std::endl;
+  std::cout << "float max:              " << FLT_MAX << std::endl;
+  std::cout << "double epsilon:         " << DBL_EPSILON << std::endl;
+  std::cout << "double min:             " << DBL_MIN << std::endl;
+  std::cout << "double max:             " << DBL_MAX << std::endl;
+  std::cout << std::endl;
+
+  // this needs #include <limits>
+  std::cout << "Also an option: std::numeric_limits<type>::max()" << std::endl;
+  std::cout << "   long minimum:           " << std::numeric_limits<long>::min() << std::endl;
+  std::cout << "   long maximum:           " << std::numeric_limits<long>::max() << std::endl;
+  std::cout << "   double max:             " << std::numeric_limits<double>::max() << std::endl;
+
+
+  return 0;
+}
+
+
