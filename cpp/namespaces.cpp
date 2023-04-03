ae38b031d0fa3ae18ccdb7c1f2b596d56468319b
mladenivkovic <mladen.ivkovic@hotmail.com>
2023-04-03 20:01:23 +0100

    extended Namespaces

--- cpp/namespaces.cpp
+++ cpp/namespaces.cpp
@@ -1,5 +1,8 @@
 #include <iostream> // IO library
 
+#include "namespaces_header.h"
+
+
 // using namespace std; // skip this for now to explicitly trace where you get things from
 
 // NOTE: the "::" in e.g. "std::cout" is the 'scope resolution operator'
@@ -17,17 +20,28 @@ namespace some_namespace_with_nameclash {
 }
 
 
+namespace some_namespace_split_among_multiple_files {
+  // define part of the namespace here in this file,
+  // and part of it in the namespace_header.h file
+  int var1 = 10;
+}
+
+
 // Use/ "import" name spaces.
 using namespace some_namespace_with_nameclash;
 
 int main(){
-
 
   // This fails due to the name clash in the namespaces. gcc won't compile it and throws an error.
   // std::cout << "Global nameclash var: " << global_nameclash_var;
 
   std::cout << "Global nameclash var: " << some_namespace_with_nameclash::global_nameclash_var << std::endl;
   std::cout << "Global nameclash var: " << ::global_nameclash_var << std::endl; // this uses the global namespace.
+
+
+  std::cout << "Split namespace var1: " << some_namespace_split_among_multiple_files::var1 << std::endl;
+  std::cout << "Split namespace var2: " << some_namespace_split_among_multiple_files::var2 << std::endl;
+
   return 0;
 }
 
---
+++ cpp/namespaces_header.h
@@ -0,0 +1,10 @@
+#ifndef NAMESPACES_HEADER_H
+#define NAMESPACES_HEADER_H
+
+namespace some_namespace_split_among_multiple_files {
+  // define part of the namespace here in this header,
+  // and part of it in the main .cpp file
+  int var2 = 20;
+}
+
+#endif
