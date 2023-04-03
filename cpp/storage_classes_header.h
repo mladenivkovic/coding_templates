4a618b5753a3e5960fe3de0ca115d1a0b199f007
mladenivkovic <mladen.ivkovic@hotmail.com>
2023-04-03 16:17:23 +0100

    updates with storage classes

---
+++ cpp/storage_classes_header.h
@@ -0,0 +1,20 @@
+#ifndef STORAGE_CLASSES_HEADER
+#define STORAGE_CLASSES_HEADER
+
+// Contains some declarations and definitions to demonstrate
+// how to use some storage classes.
+
+
+int extern_header_int = 24;
+
+/**
+ * A static function is only available in the scope of the file
+ * where it is defined.
+ */
+static void some_static_function_from_header(void){
+  std::cout << "called some static function from header" << std::endl;
+}
+
+
+
+#endif
---
+++ cpp/storage_classes_second_file.cpp
@@ -0,0 +1,27 @@
+// Define a variable in this second file, so you
+// can "import" it using the `extrnal` keyword
+
+// Note that for this to work, you need to compile this file,
+// but not link it yet (-c flag for gcc). Then include the 
+// resulting object file in the call command when compiling
+// the main `storage_classes.cpp` file.
+//
+// I.e. 
+// g++ storage_classes_second_file.cpp -c -o storage_classes_second_file.o
+// g++ storage_classes.cpp storage_classes_second_file.o -o storage_classes.o
+
+#include <iostream> // IO library
+
+int extern_int = 20;
+
+int some_int_function(int a){
+  std::cout << "some_int_function called" << std::endl;
+  return a++;
+}
+
+/**
+ * This function can't be called from storage_classes.cpp
+ */
+static void some_second_static_function(void){
+  std::cout << "called some second static function" << std::endl;
+}
