a8a4cb68ab75efdabcabac7646ffabcbf47d0519
mladenivkovic <mladen.ivkovic@hotmail.com>
2023-10-04 15:46:22 +0100

    added initializations in header material

--- cpp/header_for_initializations.h
+++ cpp/header_for_initializations.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 
 
 
@@ -10,12 +11,25 @@ public:
 
   // non-const static data member must be initialized out of line
   // can't do this: static double a_static = 1.0;
+  // You gotta initialize this static var somewhere else.
   static double static_var;
 
   // But this works
   inline const static double inline_const_static_var = 2.0;
 
+  std::string s = "Hello";
 
+  std::string sarr[3] = {"Foo", "Bar", "Baz"};
+
+  // Print out sarr
+  void print_sarr(void){
+    std::cout << "sarr = " << std::endl;
+    int test = sizeof(sarr) / sizeof(std::string);
+    std::cout << "test = " << test << std::endl;
+    for (size_t i = 0; i < sizeof(sarr)/sizeof(std::string); i++){
+      std::cout << "       " << sarr[i] << std::endl;
+    }
+  }
 };
 
 
