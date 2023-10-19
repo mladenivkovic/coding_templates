e89b23ea8dfaab84d0c1c0680110eed38be29d26
mladenivkovic <mladen.ivkovic@hotmail.com>
2023-10-19 12:20:09 +0100

    applied formatting tool

--- cpp/functions_as_function_arguments.cpp
+++ cpp/functions_as_function_arguments.cpp
@@ -3,26 +3,17 @@
 #include <functional> // needed for std::function
 #include <iostream>
 
+int add(int a, int b) { return a + b; }
 
+int multiply(int a, int b) { return a * b; }
 
-int add(int a, int b){
-  return a + b;
-}
-
-int multiply(int a, int b){
-  return a * b;
-}
-
-
-int invoke_via_pointer(int a, int b, int (*func)(int, int)){
+int invoke_via_pointer(int a, int b, int (*func)(int, int)) {
   return func(a, b);
 }
 
-int invoke_via_std_function(int a, int b, std::function<int(int, int)>func){
+int invoke_via_std_function(int a, int b, std::function<int(int, int)> func) {
   return func(a, b);
 }
-
-
 
 int main(void) {
 
@@ -30,10 +21,12 @@ int main(void) {
   int b = 3;
 
   std::cout << "Calling add: " << invoke_via_pointer(a, b, &add) << "\n";
-  std::cout << "Calling multiply: " << invoke_via_pointer(a, b, &multiply) << "\n";
+  std::cout << "Calling multiply: " << invoke_via_pointer(a, b, &multiply)
+            << "\n";
 
   std::cout << "Calling add: " << invoke_via_std_function(a, b, &add) << "\n";
-  std::cout << "Calling multiply: " << invoke_via_std_function(a, b, &multiply) << "\n";
+  std::cout << "Calling multiply: " << invoke_via_std_function(a, b, &multiply)
+            << "\n";
 
   return 0;
 }
