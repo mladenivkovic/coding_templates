c1e9b5270dc709ba7a770fd0e603370706cff64b
mladenivkovic <mladen.ivkovic@hotmail.com>
2024-04-11 16:14:03 +0100

    ran formatting script

--- python/python3/encrypt_decrypt.py
+++ python/python3/encrypt_decrypt.py
@@ -22,11 +22,15 @@ key = b"this is my key with a very long length."
 # The key needs to multiple of 16 in length.
 # So we need a Key Derivation Function.
 
+
 def derive_key(key, salt=SALT, iterations=ITERATIONS):
 
-    key_derived = Crypto.Protocol.KDF.PBKDF2(key, salt, dkLen=16, count=iterations, prf=None, hmac_hash_module=None)
+    key_derived = Crypto.Protocol.KDF.PBKDF2(
+        key, salt, dkLen=16, count=iterations, prf=None, hmac_hash_module=None
+    )
 
     return key_derived
+
 
 key_derived = derive_key(key)
 
@@ -39,13 +43,12 @@ cipher_encrypt = AES.new(key_derived, AES.MODE_CFB, iv=b"vector of size16")
 encrypted_data = cipher_encrypt.encrypt(data)
 
 # encode it to be written down.
-iv = b64encode(cipher_encrypt.iv).decode('utf-8')
-ed = b64encode(encrypted_data).decode('utf-8')
+iv = b64encode(cipher_encrypt.iv).decode("utf-8")
+ed = b64encode(encrypted_data).decode("utf-8")
 
-result = {'iv':iv, 'ciphertext':ed}
+result = {"iv": iv, "ciphertext": ed}
 
 print("Encrypted:", result)
-
 
 
 # -----------------
@@ -62,5 +65,3 @@ try:
 
 except (ValueError, KeyError):
     print("Incorrect decryption")
-
-
