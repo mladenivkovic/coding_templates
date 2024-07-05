18bd2e1e2ebb7def6ffcec53516a83872fa7a3ba
mladenivkovic <mladen.ivkovic@hotmail.com>
2024-07-05 16:59:13 +0100

    fixed move particle unit test. needed adaptation since shifting coordinates has been moved out of addEvent

--- cpp/applications/map_of_lists_of_particle_histories/main.cpp
+++ cpp/applications/map_of_lists_of_particle_histories/main.cpp
@@ -19,21 +19,20 @@ int main(void) {
   bool verbose = true;
 
   toolbox::particles::assignmentchecks::tests::TestHelpers runner = toolbox::particles::assignmentchecks::tests::TestHelpers();
-  // runner.testTruthTableSearchAndIDKeys(verbose);
-  // runner.testAddingSweepsToDatabase(verbose);
-  // runner.testAddingParticleEvents(verbose);
+  runner.testTruthTableSearchAndIDKeys(verbose);
+  runner.testAddingSweepsToDatabase(verbose);
+  runner.testAddingParticleEvents(verbose);
   // test shifting particle identifier without trimming database
-  // runner.testAddingParticleMovingEvents(100, 1000, verbose);
+  runner.testAddingParticleMovingEvents(100, 1000, verbose);
   // test shifting particle identifier and trimming database
-  // runner.testAddingParticleMovingEvents(100, 16, verbose);
+  runner.testAddingParticleMovingEvents(100, 16, verbose);
 
-  // runner.testParticleWalkSameTreeLevel();
-  // runner.testParticleLiftDrop();
-  // runner.testParticleWalk();
-  // runner.testPeriodicBoundaryConditions();
+  runner.testParticleWalkSameTreeLevel();
+  runner.testParticleLiftDrop();
+  runner.testParticleWalk();
+  runner.testPeriodicBoundaryConditions();
 
   runner.testSieveSet();
-
 
   std::cout << "Done. Bye!" <<  std::endl;
 
