97c93fa2b0bbf9da5a4ba5ab366310337dec9f0b
mladenivkovic <mladen.ivkovic@hotmail.com>
2024-07-07 12:22:14 +0100

    added singleton distributed over files

---
+++ cpp/applications/singleton_in_different_file/do_stuff.cpp
@@ -0,0 +1,38 @@
+#include "do_stuff.h"
+#include "singleton.h"
+
+#include <iostream>
+
+
+void doStuff::foo(){
+
+  singleton::S& s = singleton::S::getInstance();
+  std::cout << "In foo: Got singleton ref at         " << &s << std::endl;
+
+  s.getVar();
+  s.getOtherVar();
+  s.setVar(17);
+  s.setOtherVar(18);
+  s.getVar();
+  s.getOtherVar();
+
+}
+
+namespace doStuff{
+  singleton::S& s_outer = singleton::S::getInstance();
+}
+
+void doStuff::bar(){
+
+  std::cout << "In foo: Got singleton ref at         " << &s_outer << std::endl;
+
+  s_outer.getVar();
+  s_outer.getOtherVar();
+  s_outer.setVar(170);
+  s_outer.setOtherVar(180);
+  s_outer.getVar();
+  s_outer.getOtherVar();
+
+}
+
+
---
+++ cpp/applications/singleton_in_different_file/singleton.h
@@ -0,0 +1,45 @@
+#pragma once
+
+
+namespace singleton{
+
+class S
+{
+    public:
+        static S& getInstance()
+        {
+            static S    instance; // Guaranteed to be destroyed.
+                                  // Instantiated on first use.
+            return instance;
+        }
+    private:
+
+        int _someVar;
+        int _someOtherVar;
+
+        // Constructor? (the {} brackets) are needed here, even if you leave it empty.
+        S() {
+          _someVar = -123;
+          _someOtherVar = -456;
+        }
+
+    public:
+        // C++ 11 way of doing things. Explicitly delete the methods
+        // we don't want.
+        S(S const&)               = delete;
+        void operator=(S const&)  = delete;
+
+        // Note: Scott Meyers mentions in his Effective Modern
+        //       C++ book, that deleted functions should generally
+        //       be public as it results in better error messages
+        //       due to the compilers behavior to check accessibility
+        //       before deleted status
+
+        void setVar(int x);
+        void setOtherVar(int x);
+        int getVar() const;
+        int getOtherVar() const;
+};
+
+
+} // namespace singleton
