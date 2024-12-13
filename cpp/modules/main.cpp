49af09333371f19f4fd6807d2c7909f1f08db095
mladenivkovic <mladen.ivkovic@hotmail.com>
2024-12-13 01:37:10 +0000

    added modules

---
+++ cpp/modules/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+import my_module;
+
+int main(void){
+
+  int result = add(3, 6);
+  result = multiply(2, 4);
+
+  std::cout << "Hello world! Result=" << result << "\n";
+
+  return 0;
+}
