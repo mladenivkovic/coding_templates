4c978eac22ed6f85e13bc2d2a60651f0c11b8dd9
mladenivkovic <mladen.ivkovic@hotmail.com>
2025-11-24 23:21:09 +0000

    added reading and writing binary

---
+++ c/applications/readwrite_binary/data_struct.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* Struct containing data that we'll write out */
+struct output_data {
+  int index;
+  int some_int;
+  char some_char;
+  float some_float;
+  double some_double;
+};
+
