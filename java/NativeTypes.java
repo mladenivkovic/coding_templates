0573cdd6907ed44a341747618bf31de98b81b73d
mladenivkovic <mladen.ivkovic@hotmail.com>
2026-06-25 11:22:01 +0100

    adding native types file

---
+++ java/NativeTypes.java
@@ -0,0 +1,20 @@
+class NativeTypes2 {
+
+  public static void main(){
+
+    char myChar = 'a';
+
+    int myInt = 234;
+    float myFloat = 2.34f;
+    double myDouble = 3.45;
+
+    String myString = "Hello World!";
+
+    System.out.println(myChar);
+    System.out.println(myInt);
+    System.out.println(myFloat);
+    System.out.println(myDouble);
+    System.out.println(myString);
+
+  }
+}
